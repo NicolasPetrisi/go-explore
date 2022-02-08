f35292379d6467fbcf74b4b8cd3f79b527f630dd
fr8272sj-s <fr8272sj-s@student.lu.se>
2022-02-08 17:16:17 +0100

    tryin again

---
+++ image_processing/atari.py
@@ -0,0 +1,25 @@
+import gym
+from gym.wrappers import Monitor
+import numpy as np
+import cv2
+
+def to_np(array):
+    arr = cv2.imencode('.png', array, [cv2.IMWRITE_PNG_COMPRESSION, 1])[1].flatten().tobytes()
+    print(arr)
+    print(cv2.imdecode(np.frombuffer(arr, np.uint8), 0))
+    return cv2.imdecode(np.frombuffer(array, np.uint8), 0)
+
+env = gym.make('AlienNoFrameskip-v4')
+#env = Monitor(env, './video', force = True)
+for episode in range(5):
+    observation = env.reset()
+    print(observation.shape)
+    done = False
+    score = 0
+    while not done:
+        action = env.action_space.sample()
+        observation, reward, done, info  = env.step(action)
+        score += reward
+    print( " score  " + str(score))
+    print(to_np(observation))
+env.close()
