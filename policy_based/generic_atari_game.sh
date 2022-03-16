1df739dbe7098cf612ae35c56fdfa3bf937c007a
fr8272sj-s <fr8272sj-s@student.lu.se>
2022-03-16 13:48:37 +0100

    now goal is the final goal instead

--- policy_based/generic_atari_game.sh
+++ policy_based/generic_atari_game.sh
@@ -4,7 +4,7 @@
 # For the full experiment settings, change each setting to each "full experiment" value.
 
 # Full experiment: 16
-NB_MPI_WORKERS=8
+NB_MPI_WORKERS=1
 # Full experiment: 16
 NB_ENVS_PER_WORKER=2
 
@@ -81,7 +81,7 @@ CELL_REPRESENTATION_OPTIONS="--cell_representation generic" #TODO change this, s
 # When the the final cell is reached, there is a random_exp_prob (0.5) chance that we explore by taking random actions, rather than by sampling from the policy.
 EPISODE_OPTIONS="--trajectory_tracker sparse_soft --soft_traj_win_size 10 --random_exp_prob 0.0 --delay 0"
 
-CHECKPOINT_OPTIONS="--checkpoint_compute ${CHECKPOINT} --clear_checkpoints trajectory"
+CHECKPOINT_OPTIONS="--checkpoint_compute ${CHECKPOINT} --clear_checkpoints trajectory --final_goal_or_sub_goal final_goal"
 TRAINING_OPTIONS="--goal_rep raw --gamma 0.99 --learning_rate=2.5e-4 --no_exploration_gradients --sil=sil --max_compute_steps 12000000000" #"--goal_rep onehot_r24 should probally be --goal_rep onehot
-MISC_OPTIONS="--low_prob_traj_tresh 0.01 --start_method fork --log_info INFO --log_files __main__ ${Load} ${MaxTime} ${MaxSteps} ${LevelSeed} ${PosSeed}"
+MISC_OPTIONS="--low_prob_traj_tresh 0.01 --start_method fork --log_info INFO --log_files __main__ ${Load} ${MaxTime} ${MaxSteps} ${LevelSeed} ${PosSeed} --video_all_ep True"
 mpirun -n ${NB_MPI_WORKERS} python3 goexplore_start.py --base_path ~/temp --nb_envs ${NB_ENVS_PER_WORKER} ${REWARD_OPTIONS} ${CELL_SELECTION_OPTIONS} ${ENTROPY_INC_OPTIONS} ${CHECKPOINT_OPTIONS} ${CELL_REPRESENTATION_OPTIONS} ${EPISODE_OPTIONS} ${GAME_OPTIONS} ${TRAINING_OPTIONS} ${MISC_OPTIONS}
