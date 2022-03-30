f2db75f7e473c80d034fa9cefd100d8b711c158f
nicolas <nicolas97p@hotmail.com>
2022-03-30 16:47:42 +0200

    plot_log now plots 4 equally large plots from log files of unequal length

--- image_processing/plot_log.py
+++ image_processing/plot_log.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
 import os
 
 def make_sub_list(input_list,seperator):
@@ -16,7 +17,6 @@ def get_values(filepath, x_name, y_name):
     with open(filepath) as f:
         lines = f.readlines()
         first = make_sub_list(lines, ',')
-
         x_index = first[0].index(x_name)
         y_index = first[0].index(y_name)
         x_values = []
@@ -28,12 +28,12 @@ def get_values(filepath, x_name, y_name):
                 y_values.append(float(line[y_index]))
         return x_values,y_values
 
-def plot_values(x_values, y_values, x_label, y_label, seed, name="plot.png"):
+def plot_values(x_values, y_values, x_label, y_label, title, name="plot.png"):
     plt.clf()
     plt.plot(x_values, y_values)
     plt.ylabel(y_label)
     plt.xlabel(x_label)
-    plt.title("Level Seed = " + str(seed))
+    plt.title(title)
     plt.savefig(name)
 
 def main():
@@ -41,16 +41,29 @@ def main():
     if not os.path.isdir('plots'):
         os.mkdir('plots')
 
-    seed = 0
-    x_axis_label = "frames"
-    y_axis_label = "cells"
-    x, y = get_values(str(sys.argv[1]), x_axis_label, y_axis_label)
-    plot_values(x, y, x_axis_label, y_axis_label, seed, "plots/" + y_axis_label + "_of_" + x_axis_label + ".png")
 
-    seed = 0
+    files = []
+
+    for i in range(1, 5):
+        files.append(sys.argv[i])
+
     x_axis_label = "frames"
     y_axis_label = "ret_suc"
-    x, y = get_values(str(sys.argv[1]), x_axis_label, y_axis_label)
-    plot_values(x, y, x_axis_label, y_axis_label, seed, "plots/" + y_axis_label + "_of_" + x_axis_label + ".png")
+    x_values = []
+    y_values = []
+    min_len = sys.maxsize
+    for file in files:
+        x, y = get_values(file, x_axis_label, y_axis_label)
+        x_values.append(x)
+        y_values.append(y)
+        min_len = np.min([min_len, len(x)])
+
+    for i in range(len(x_values)):
+        x_values[i] = x_values[i][:min_len]
+        y_values[i] = y_values[i][:min_len]
+
+    for i in range(len(x_values)):
+        plot_values(x_values[i], y_values[i], x_axis_label, y_axis_label, files[i], "plots/" + files[i] + "_" + y_axis_label + "_of_" + x_axis_label + ".png")
+
 
 main()
