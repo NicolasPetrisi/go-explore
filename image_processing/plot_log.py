5d44a247f9fefb5485ba0a2d75cca4b44bb10667
nicolas <nicolas97p@hotmail.com>
2022-04-27 16:56:28 +0200

    Added the newer version of plotlog to master

--- image_processing/plot_log.py
+++ image_processing/plot_log.py
@@ -1,6 +1,33 @@
+from importlib_metadata import DistributionFinder
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
 import os
+
+word_dict = dict()
+
+word_dict['it'] = 'Iteration'
+word_dict['ret_suc'] = 'Return Sucess'
+word_dict['ret_cum_suc'] = 'Cumulative Return Success'
+word_dict['exp_suc'] = 'Exploration Success'
+word_dict['opt_len'] = 'Optimal Length'
+word_dict['dist_from_opt'] = 'Distance from Optimal Length'
+word_dict['len_mean'] = 'Length Mean'
+word_dict['frames'] = 'Frames'
+word_dict['rew_mean'] = 'Reward Mean'
+word_dict['score'] = 'Score'
+word_dict['ep'] = 'Episode'
+word_dict['cells'] = 'Cells'
+word_dict['arch_suc'] = 'Archive Success'
+word_dict['policy_loss'] = 'Policy Loss'
+word_dict['value_loss'] = 'Value Loss'
+word_dict['l2_loss'] = 'L2 Loss'
+word_dict['policy_entropy'] = 'Policy Entropy'
+word_dict['cells_found_ret'] = 'Cells Found when Returning'
+word_dict['cells_found_rand'] = 'Cells Found when Random Exploring'
+word_dict['cells_found_policy'] = 'Cells Found when Policy Exploring'
+
+
 
 def make_sub_list(input_list,seperator):
     final = []
@@ -16,7 +43,6 @@ def get_values(filepath, x_name, y_name):
     with open(filepath) as f:
         lines = f.readlines()
         first = make_sub_list(lines, ',')
-
         x_index = first[0].index(x_name)
         y_index = first[0].index(y_name)
         x_values = []
@@ -28,29 +54,57 @@ def get_values(filepath, x_name, y_name):
                 y_values.append(float(line[y_index]))
         return x_values,y_values
 
-def plot_values(x_values, y_values, x_label, y_label, seed, name="plot.png"):
+def plot_values(word_dict, x_values, y_values, x_label, y_label, title, name="plot.png"):
     plt.clf()
     plt.plot(x_values, y_values)
-    plt.ylabel(y_label)
-    plt.xlabel(x_label)
-    plt.title("Level Seed = " + str(seed))
+    if y_label in word_dict:
+        label_name = word_dict[y_label]
+    else:
+        label_name = y_label
+        print("Y-label name was not in the word dictionary! Using log file label name instead")
+    plt.ylabel(label_name)
+
+    if x_label in word_dict:
+        label_name = word_dict[x_label]
+    else:
+        label_name = x_label
+        print("X-label name was not in the word dictionary! Using log file label name instead")
+    plt.xlabel(label_name)
+    
+    # NOTE: If you want a fixed y-axis. Use this line!
+    #plt.ylim([0,1])
+    
+    plt.title(title)
     plt.savefig(name)
 
-def main():
+def main(word_dict):
 
     if not os.path.isdir('plots'):
         os.mkdir('plots')
 
-    seed = 0
-    x_axis_label = "frames"
-    y_axis_label = "cells"
-    x, y = get_values(str(sys.argv[1]), x_axis_label, y_axis_label)
-    plot_values(x, y, x_axis_label, y_axis_label, seed, "plots/" + y_axis_label + "_of_" + x_axis_label + ".png")
 
-    seed = 0
+    files = []
+
+    for i in range(1, len(sys.argv)):
+        files.append(sys.argv[i])
+
     x_axis_label = "frames"
     y_axis_label = "ret_suc"
-    x, y = get_values(str(sys.argv[1]), x_axis_label, y_axis_label)
-    plot_values(x, y, x_axis_label, y_axis_label, seed, "plots/" + y_axis_label + "_of_" + x_axis_label + ".png")
+    x_values = []
+    y_values = []
+    min_len = sys.maxsize
+    for file in files:
+        x, y = get_values(file, x_axis_label, y_axis_label)
+        x_values.append(x)
+        y_values.append(y)
+        min_len = np.min([min_len, len(x)])
 
-main()
+    for i in range(len(x_values)):
+        x_values[i] = x_values[i][:min_len]
+        y_values[i] = y_values[i][:min_len]
+
+    for i in range(len(x_values)):
+        plot_values(word_dict, x_values[i], y_values[i], x_axis_label, y_axis_label, files[i].split('.')[0], "plots/" + files[i].split('.')[0] + "_" + y_axis_label + "_of_" + x_axis_label + ".png")
+
+
+main(word_dict)
